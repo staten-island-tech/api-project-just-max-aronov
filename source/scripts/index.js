7a052562a1fdbcc0efdf16d3d87256fff0f4909e
Requias <73113726+MaxAronov@users.noreply.github.com>
2021-04-23 21:31:14 -0400

    Navigation bar done

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,8 +3,6 @@ import 'regenerator-runtime/runtime'
 
 const octokit = new Octokit();
 
-repoFullfilled();
-
 window.onkeyup = keyup;
 
 let inputTextValue;
@@ -23,13 +21,6 @@ function userObjectObtain() {
     });
 }
 
-function repoObjectObtain() {
-    return octokit.rest.repos.get({
-        owner: "JustReq",
-        repo: "JustReq"
-    });
-}
-
 async function userFullfilled() {
     const userObject = await (await userObjectObtain()).data
     let userName = userObject.login;
@@ -44,7 +35,36 @@ async function userFullfilled() {
     document.getElementById("user-information").innerHTML = `Username: ${userName} \"${nickName}\" <br>Avatar: ${avatarURL}<br>Status: \"${bio}\"<br>Created at ${creationDate}<br>Has ${followers} followers<br>Follwing ${following} users<br>Profile: ${profileURL}<br>${repoCount} public repositories`;
 }
 
-async function repoFullfilled() {
-    const repoObject = await (await repoObjectObtain()).data
-    console.log(repoObject)
-}
+document.getElementById("users-nav-button").addEventListener("mouseover", () => {
+    document.getElementById("tooltip-text-container").style.opacity = 1;
+    document.getElementById("tooltip-text").textContent = "Search for and view information about a user";
+})
+
+document.getElementById("users-nav-button").addEventListener("mouseout", () => {
+    document.getElementById("tooltip-text-container").style.opacity = 0;
+    document.getElementById("tooltip-text").textContent = "";
+})
+
+document.getElementById("repos-nav-button").addEventListener("mouseover", () => {
+    document.getElementById("tooltip-text-container").style.opacity = 1;
+    document.getElementById("tooltip-text").textContent = "Search for and view information about a repository";
+})
+
+document.getElementById("repos-nav-button").addEventListener("mouseout", () => {
+    document.getElementById("tooltip-text-container").style.opacity = 0;
+    document.getElementById("tooltip-text").textContent = "";
+})
+
+document.getElementById("code-nav-button").addEventListener("mouseover", () => {
+    document.getElementById("tooltip-text-container").style.opacity = 1;
+    document.getElementById("tooltip-text").textContent = "Traverse through and cringe at my spaghetti code";
+})
+
+document.getElementById("code-nav-button").addEventListener("mouseout", () => {
+    document.getElementById("tooltip-text-container").style.opacity = 0;
+    document.getElementById("tooltip-text").textContent = "";
+})
+
+setInterval(function() {
+    console.log(window["tooltip-text"])
+}, 1000);
