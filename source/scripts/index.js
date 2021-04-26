e3b60ed4521b6e57f24177b6278816fa769efc40
Requias <73113726+MaxAronov@users.noreply.github.com>
2021-04-25 21:58:01 -0400

    Starting to work with Anime.JS

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,5 +1,6 @@
 import { Octokit } from "@octokit/rest";
 import 'regenerator-runtime/runtime'
+import anime from 'animejs/lib/anime.es.js';
 
 const octokit = new Octokit();
 
@@ -26,13 +27,24 @@ async function userFullfilled() {
     let userName = userObject.login;
     let avatarURL = userObject.avatar_url;
     let bio = userObject.bio;
-    let creationDate = userObject.created_at;
+    let creationDatePreformatted = new Date(userObject.created_at);
     let followers = userObject.followers;
     let following = userObject.following;
     let profileURL = userObject.html_url;
     let nickName = userObject.name;
     let repoCount = userObject.public_repos;
-    document.getElementById("user-information").innerHTML = `Username: ${userName} \"${nickName}\" <br>Avatar: ${avatarURL}<br>Status: \"${bio}\"<br>Created at ${creationDate}<br>Has ${followers} followers<br>Follwing ${following} users<br>Profile: ${profileURL}<br>${repoCount} public repositories`;
+
+    let year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(creationDatePreformatted)
+    let month = new Intl.DateTimeFormat('en', { month: 'long' }).format(creationDatePreformatted)
+    let day = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(creationDatePreformatted)
+
+    let hour = new Intl.DateTimeFormat('en', { hour: '2-digit', hour12: false }).format(creationDatePreformatted)
+    let minute = new Intl.DateTimeFormat('en', { minute: '2-digit' }).format(creationDatePreformatted)
+    let second = new Intl.DateTimeFormat('en', { second: '2-digit' }).format(creationDatePreformatted)
+
+    let creationDateFormatted = `${month} ${day}, ${year}, at ${hour}:${minute}:${second}`
+
+    console.log(`Username: ${userName} \"${nickName}\" \nAvatar: ${avatarURL}\nStatus: \"${bio}\"\nCreated at ${creationDateFormatted}\nHas ${followers} followers\nFollowing ${following} users\nProfile: ${profileURL}\n${repoCount} public repositories`)
 }
 
 document.getElementById("users-nav-button").addEventListener("mouseover", () => {
@@ -57,7 +69,7 @@ document.getElementById("repos-nav-button").addEventListener("mouseout", () => {
 
 document.getElementById("code-nav-button").addEventListener("mouseover", () => {
     document.getElementById("tooltip-text-container").style.opacity = 1;
-    document.getElementById("tooltip-text").textContent = "Traverse through and cringe at my spaghetti code";
+    document.getElementById("tooltip-text").textContent = "Look through my spaghetti code";
 })
 
 document.getElementById("code-nav-button").addEventListener("mouseout", () => {
@@ -65,6 +77,52 @@ document.getElementById("code-nav-button").addEventListener("mouseout", () => {
     document.getElementById("tooltip-text").textContent = "";
 })
 
-setInterval(function() {
-    console.log(window["tooltip-text"])
-}, 1000);
+document.getElementById("users-nav-button").addEventListener("click", function mainPageAnimation() {
+    let userMainHide = anime.timeline({
+        easing: 'cubicBezier(0.420, 0.000, 0.580, 1.000)',
+        duration: 500
+    });
+
+    userMainHide.add({
+        targets: document.getElementById("page-header"),
+        top: 0
+    }, 0)
+
+    .add({
+        targets: document.getElementById("subtitle-container"),
+        opacity: 0
+    }, 0)
+
+    .add({
+        targets: document.getElementById("navigation-section"),
+        top: 300,
+        opacity: 0
+    }, 0)
+
+    .add({
+        targets: document.getElementById("navigation-section"),
+        top: -100
+    }, 500);
+
+    let userNewShow = anime.timeline({
+        duration: 800,
+        delay: 0
+    });
+
+    userNewShow.add({
+        targets: document.getElementById("user-search-section"),
+        left: 0
+    }, 0)
+
+    .add({
+        targets: document.getElementById("user-search-section"),
+        top: 152.5,
+        opacity: 1
+    }, 700)
+
+    .add({
+        targets: document.getElementById("user-filter-section"),
+        left: -400,
+        opacity: 1
+    }, 800)
+})
